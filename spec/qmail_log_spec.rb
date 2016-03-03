156b33d79836977968dbf2d63eef381bd65e04f2
alotofwe <u.humika@gmail.com>
2016-03-04 06:32:13 +0900

    spec for analyze logs in directory

--- spec/qmail_log_spec.rb
+++ spec/qmail_log_spec.rb
@@ -1,5 +1,23 @@
 describe QmailLog do
   it 'has a version number' do
-    expect(QmailLog::VERSION).not_to be nil
+    expect( QmailLog::VERSION ).not_to be nil
+  end
+
+  describe '.analyze' do
+    describe 'when given path is file' do
+      let(:path) { './spec/helper/files/log/one.log' }
+
+      it 'should analyze given file' do
+        expect( QmailLog.analyze(path).length ).to eq(2)
+      end
+    end
+
+    describe 'when given path is directory' do
+      let(:path) { './spec/helper/files/log' }
+
+      it 'should analyze all files in given directory' do
+        expect( QmailLog.analyze(path).length ).to eq(4)
+      end
+    end
   end
 end
