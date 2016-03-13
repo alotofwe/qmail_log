900bc592fa3b1eed5986b5494a8a3eacbea96698
alotofwe <u.humika@gmail.com>
2016-03-13 09:32:38 +0900

    Seperate into Exec and SSH from analyze_spec

---
+++ spec/analyze/ssh_spec.rb
@@ -0,0 +1,25 @@
+describe QmailLog::Analyze::SSH do
+  let(:host) { 'localhost' }
+  before {
+    FileUtils.touch('./spec/helper/files/log/one.log',         mtime: Time.now - 300)
+    FileUtils.touch('./spec/helper/files/log/two.log',         mtime: Time.now - 200)
+    FileUtils.touch('./spec/helper/files/log/three/three.log', mtime: Time.now - 100)
+    allow(QmailLog::SSH::Configure).to receive(:backend)
+  }
+
+  describe 'when given path is file' do
+    let(:path) { './spec/helper/files/log/one.log' }
+
+    it 'should analyze given file' do
+      expect( QmailLog.analyze(:ssh, host, path).length ).to eq(2)
+    end
+  end
+
+  describe 'when given path is directory' do
+    let(:path) { './spec/helper/files/log' }
+
+    it 'should analyze all files in given directory' do
+      expect( QmailLog.analyze(:ssh, host, path).length ).to eq(4)
+    end
+  end
+end
--- spec/qmail_log_spec.rb
+++ spec/qmail_log_spec.rb
@@ -2,28 +2,4 @@ describe QmailLog do
   it 'has a version number' do
     expect( QmailLog::VERSION ).not_to be nil
   end
-
-  describe '.analyze' do
-    before {
-      FileUtils.touch('./spec/helper/files/log/one.log',         mtime: Time.now - 300)
-      FileUtils.touch('./spec/helper/files/log/two.log',         mtime: Time.now - 200)
-      FileUtils.touch('./spec/helper/files/log/three/three.log', mtime: Time.now - 100)
-    }
-
-    describe 'when given path is file' do
-      let(:path) { './spec/helper/files/log/one.log' }
-
-      it 'should analyze given file' do
-        expect( QmailLog.analyze(path).length ).to eq(2)
-      end
-    end
-
-    describe 'when given path is directory' do
-      let(:path) { './spec/helper/files/log' }
-
-      it 'should analyze all files in given directory' do
-        expect( QmailLog.analyze(path).length ).to eq(4)
-      end
-    end
-  end
 end
