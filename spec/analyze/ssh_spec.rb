f86fcd836f2ff7584fb6961e5a68d4842898a35a
alotofwe <u.humika@gmail.com>
2016-03-13 09:49:19 +0900

    Give ssh_options

--- spec/analyze/ssh_spec.rb
+++ spec/analyze/ssh_spec.rb
@@ -1,5 +1,12 @@
 describe QmailLog::Analyze::SSH do
   let(:host) { 'localhost' }
+  let(:ssh_options) do
+    {
+      port:     22,
+      user:     'alotofwe',
+      password: 'Always Look on the Bright Side of Life'
+    }
+  end
   before {
     FileUtils.touch('./spec/helper/files/log/one.log',         mtime: Time.now - 300)
     FileUtils.touch('./spec/helper/files/log/two.log',         mtime: Time.now - 200)
@@ -11,7 +18,7 @@ describe QmailLog::Analyze::SSH do
     let(:path) { './spec/helper/files/log/one.log' }
 
     it 'should analyze given file' do
-      expect( QmailLog.analyze(:ssh, path, host).length ).to eq(2)
+      expect( QmailLog.analyze(:ssh, path, host, ssh_options).length ).to eq(2)
     end
   end
 
@@ -19,7 +26,7 @@ describe QmailLog::Analyze::SSH do
     let(:path) { './spec/helper/files/log' }
 
     it 'should analyze all files in given directory' do
-      expect( QmailLog.analyze(:ssh, path, host).length ).to eq(4)
+      expect( QmailLog.analyze(:ssh, path, host, ssh_options).length ).to eq(4)
     end
   end
 end
